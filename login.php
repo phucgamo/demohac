1b8eba858219559ccd687a742ef5b586ec0fb7fe
phucgamo <agpzin0.0@gmail.com>
2023-12-23 14:52:38 +0700

    login

--- login.php
+++ login.php
@@ -1,32 +1,38 @@
 <?php
-$hostname = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "sqli";
-$conn = mysqli_connect($hostname, $username, $password,$dbname );
-if(!$conn){
-    die("Unable to connect");
-}
-if($_POST){
-    $uname = $_POST["username"];
-    $pass = $_POST["password"];
-    //Preventing SQL Injection
-    //$uname = mysqli_real_escape_string($conn, $uname);
-   // $pass = mysqli_real_escape_string($conn, $uname);
-    $sql = "SELECT * FROM demo WHERE user = '$uname' AND pwd = '$pass'";
-    $result = mysqli_query($conn,$sql);
-    if(mysqli_num_rows($result) >= 1){
-        echo"Welcome, user!";
-        while( $row = mysqli_fetch_assoc( $result)){
-            // Get values
-            $id = $row["id"];
-            $uname = $row["user"];
+$dbname = "your_dbname";
 
-            //Feedback for and user
-            echo "<pre>ID: {$id}<br />Username: {$uname}</pre>";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Thực hiện truy vấn kiểm tra thông tin đăng nhập
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    // Kiểm tra xem truy vấn có thành công hay không
+    if ($result !== false) {
+        if ($result->num_rows > 0) {
+            echo "Login successful";
+            // Thực hiện các hành động sau khi đăng nhập thành công
+        } else {
+            echo "Login failed. Invalid username or password.";
         }
-    }else{
-        echo"Incorrect Username/Password";
+    } else {
+        // Hiển thị thông báo lỗi nếu truy vấn thất bại
+        die("Query failed: " . $conn->error);
     }
 }
+
+$conn->close();
 ?>
